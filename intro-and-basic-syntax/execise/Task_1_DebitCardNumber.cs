267d804ac4081e56f2bf85e1f077097596b2b84f
Yonkov <yonkov.atanas@gmail.com>
2018-03-13 23:17:25 +0200

    added problem one solution

---
+++ intro-and-basic-syntax/execise/Task_1_DebitCardNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_Debit_card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = int.Parse(Console.ReadLine());
+            int int2 = int.Parse(Console.ReadLine());
+            int int3 = int.Parse(Console.ReadLine());
+            int int4 = int.Parse(Console.ReadLine());
+
+            Console.Write("{0:d4} ", int1);
+            Console.Write("{0:d4} ", int2);
+            Console.Write("{0:d4} ", int3);
+            Console.Write("{0:d4} ", int4);
+        }
+    }
+}
