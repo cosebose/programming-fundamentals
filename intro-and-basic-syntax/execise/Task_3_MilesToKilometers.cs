1983d4b09d0ed1b93ae54150d2d3f4a6dc8a0303
alex2208 <37144355+alex2208@users.noreply.github.com>
2018-03-17 00:40:19 +0200

    Task 3 added

---
+++ intro-and-basic-syntax/execise/Task_3_MilesToKilometers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 mile == 1.60934 kilometers
+            double miles = int.Parse(Console.ReadLine());
+            double kilometres = (miles * 1.60934);
+            Console.WriteLine($"{kilometres:F2}");
+
+        }
+    }
+}
