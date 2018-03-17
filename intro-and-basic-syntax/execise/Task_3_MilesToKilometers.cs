d0482f454209dfd841cdc9d17983e253036817e3
Georgi Anastasov <gogata258@gmail.com>
2018-03-17 15:13:12 +0200

    Update Task_3_MilesToKilometers.cs

--- intro-and-basic-syntax/execise/Task_3_MilesToKilometers.cs
+++ intro-and-basic-syntax/execise/Task_3_MilesToKilometers.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//confirmed by gogata258
+using System;
 
 namespace ConsoleApp1
 {
