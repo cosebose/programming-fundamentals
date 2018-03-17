08bdd4b5dfe04e8693f05a3c72aabd67bc598633
alex2208 <37144355+alex2208@users.noreply.github.com>
2018-03-17 12:49:16 +0200

    changed var name from int1-4 to redica1-4

--- intro-and-basic-syntax/execise/Task_1_DebitCardNumber.cs
+++ intro-and-basic-syntax/execise/Task_1_DebitCardNumber.cs
@@ -6,10 +6,10 @@ namespace _01_Debit_card
     {
         static void Main(string[] args)
         {
-            int int1 = int.Parse(Console.ReadLine());
-            int int2 = int.Parse(Console.ReadLine());
-            int int3 = int.Parse(Console.ReadLine());
-            int int4 = int.Parse(Console.ReadLine());
+            int redica1 = int.Parse(Console.ReadLine());
+            int redica2 = int.Parse(Console.ReadLine());
+            int redica3 = int.Parse(Console.ReadLine());
+            int redica4 = int.Parse(Console.ReadLine());
 
             Console.Write("{0:d4} ", int1);
             Console.Write("{0:d4} ", int2);
